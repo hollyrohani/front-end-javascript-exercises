7c643d79ac12a140700935a97d27c26e039ad994
hollyrohani <hollyrohani@gmail.com>
2015-04-30 15:40:14 -0700

    Arrays

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -1,3 +1,14 @@
-module.exports.newArray = undefined;
+module.exports.newArray = function (one, two, three, four) {
+	return [one, two, three, four];
+};
 
-module.exports.firstAndLast = undefined;
+
+module.exports.firstAndLast = function(array) {
+	/*
+		take one argument, an array with at least 3 elements
+		return a new array with the first and last element of the passed array
+	*/
+	var first = array[0];
+	var last = array[array.length - 1];
+	return [first, last];
+}
