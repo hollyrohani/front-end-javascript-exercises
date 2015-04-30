bea2748b44ee863d82c49fc27763a78776a37c3e
hollyrohani <hollyrohani@gmail.com>
2015-04-30 16:08:44 -0700

    Arrays

--- 08-arrays/01_Array_Functions/challenge.js
+++ 08-arrays/01_Array_Functions/challenge.js
@@ -1,5 +1,23 @@
-module.exports.reversePlusOne = undefined;
+module.exports.reversePlusOne = function(test) {
+	test.reverse();
+	test.unshift(1);
+	return test;
+}
 
-module.exports.plusesEverywhere = undefined;
+module.exports.plusesEverywhere = function(test) {
+	return test.join("+");
+}
+	/* Take one argument, an array of at least two numbers.
+This function should return:
+a String made of all the values in the array separated by + 
+	*/ 
 
-module.exports.arrayQuantityPlusOne = undefined;
+module.exports.arrayQuantityPlusOne = function(test) {
+	// return a number that is equal to the number of items in the array plus one
+	var number = test.length;
+	return number + 1;
+}
+/* Take one argument, an array of numbers.
+This function should return:
+one greater than the number of items in the array
+*/
--- 08-arrays/02_Grocery_List/challenge.js
+++ 08-arrays/02_Grocery_List/challenge.js
@@ -1,3 +1,14 @@
-module.exports.addItem = undefined;
+module.exports.addItem = function(itemToAdd, array) {
+	// indexOf will return the index that the itemToAdd is found in the array if it is in the array, or -1 if it is not
+	// [1,2,3].indexOf(2) === 1 // found
+	// [1,2,3].indexOf("a") === -1 // not found
+	if (array.indexOf(itemToAdd) === -1) {
+		array.push(itemToAdd);
+	}
+	
+	return array;
+}
 
-module.exports.reverseSortedList = undefined;
+module.exports.reverseSortedList = function(array) {
+	return array.sort().reverse();
+}
