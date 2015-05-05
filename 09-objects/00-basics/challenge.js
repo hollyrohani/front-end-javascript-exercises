67524c069a67750ffe80ae9ff344e4195d12f3b1
hollyrohani <hollyrohani@gmail.com>
2015-05-04 17:16:11 -0700

    Objects

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,51 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(courseTitle, courseDuration, courseStudents) {
+	var obj = {
+		title: courseTitle,
+		duration: courseDuration,
+		students: courseStudents
+	};
+	return obj;
+};
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function(object, newProp, newValue) {
+	// if the object does not have the property already
+	if (!object.hasOwnProperty(newProp)) {
+		// add the property with a given value
+		object[newProp] = newValue;
+	}
+	
+	return object;
+}
 
-module.exports.formLetter = undefined;
+module.exports.formLetter = function(letter) {
+	var letter = {
+  	recipient: "David",
+  	msg: "What up, thug?",
+  	sender: "Arnold"
+  };
+  return letter = "Hello " + letter.recipient + ",\n\n" + letter.msg + "\n\nSincerely,\n" + letter.sender;
+}
 
-module.exports.canIGet = undefined;
+module.exports.canIGet = function(item, money) {
+	// item === "MacBook Air"
+	// money === 1000
+	var items = {
+		"MacBook Air": 999,
+		"MacBook Pro": 1299,
+		"Mac Pro": 2499,
+		"Apple Sticker": 1
+	};
+	
+	// var price = items["MacBook Air"]; // 999
+	var price = items[item]; // undefined obj["MacBook Air 2"] does not exist
+	
+	/* with if
+	if (typeof price !== "undefined" && money >= price) {
+		return true;
+	} else {
+		return false;
+	} */
+	
+	/* without if */
+	return typeof price !== "undefined" && money >= price;
+}
